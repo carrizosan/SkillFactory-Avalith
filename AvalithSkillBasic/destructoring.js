a4170efd59cec3804eb5c4ea6d6a36e2670adf06
carrizosan <santiicaarrizo@gmail.com>
2021-07-25 20:40:15 -0300

    Ejercicio Destructuring

--- AvalithSkillBasic/destructoring.js
+++ AvalithSkillBasic/destructoring.js
@@ -3,17 +3,15 @@ const original = {
     b: 2,
     c: 3,
 };
-// -------
+
+
 let sinB = null;
 let valorDeC = null;
 
-/*
-    Utilizando SOLAMENTE destructoring, obtener los siguientes resultados:
-    - sinB: Una copia del objeto original sin la clave "b"
-    - valorDeC: Obtener el valor de la clave "c". (¡No puede hacer "valorDeC = original.c"!)
+let {a, c} = original;
 
-    Reemplazar este comentario con su codigo.
- */
+sinB = {a, c};
+valorDeC = c
 
 console.log(sinB);
 console.log(valorDeC);
