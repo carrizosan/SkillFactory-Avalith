9a65664e430fb518889d61a53c86cd1bca0f9b49
carrizosan <santiicaarrizo@gmail.com>
2021-07-25 21:09:33 -0300

    Tarea regex

--- AvalithSkillBasic/regex.js
+++ AvalithSkillBasic/regex.js
@@ -1,13 +1,7 @@
 const texto = 'Mar del Plata​ es una ciudad ubicada en el sudeste de la provincia de Buenos Aires';
 let textoCensurado = null;
 
-/*
-    Utilizando una expresion regular, reemplace todas las letras del texto con "X",
-    de manera que se muestre por consola:
 
-    "XXX XXX XXXXX​ XX XXX XXXXXX XXXXXXX XX XX XXXXXXX XX XX XXXXXXXXX XX XXXXXX XXXXX"
-
-    Reemplazar este comentario con su codigo.
- */
+textoCensurado = texto.replace(/\w/g, "X");
 
 console.log(textoCensurado)
